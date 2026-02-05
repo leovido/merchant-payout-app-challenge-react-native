914b4de326792a61c541cafdda8a029dc3530362
Christian Ray Leovido <18484997+leovido@users.noreply.github.com>
2026-02-05 12:25:29 +0100

    refactor: use compound components

--- components/ActivityListItem.tsx
+++ components/ActivityListItem.tsx
@@ -1,6 +1,6 @@
 import { StyleSheet, View } from "react-native";
 import type { Currency } from "@/types/api";
-import { currencyFormatter } from "@/utils/formatter";
+import { formatCurrency } from "@/utils/formatter";
 import { ThemedText } from "./themed-text";
 
 interface ActivityListItemProps {
@@ -9,46 +9,53 @@ interface ActivityListItemProps {
 	amount: number;
 }
 
-const formatCurrency = (amount: number, currency: Currency): string => {
-	return new Intl.NumberFormat("en-GB", {
-		style: "currency",
-		currency: currency,
-	}).format(amount);
-};
-
 export function ActivityListItem({
 	description,
 	currency,
 	amount,
 }: ActivityListItemProps) {
-	const formattedCurrency = currencyFormatter(currency);
 	const formattedAmount = formatCurrency(amount, currency);
 
 	return (
 		<View style={styles.container}>
-			<ThemedText
-				accessibilityLabel="Activity description"
-				accessibilityRole="text"
-				accessibilityValue={{ text: description }}
-			>
-				{description}
-			</ThemedText>
-			<ThemedText
-				type="defaultSemiBold"
-				style={[
-					styles.amount,
-					amount > 0 ? styles.positiveAmount : styles.negativeAmount,
-				]}
-				accessibilityLabel="Activity amount"
-				accessibilityRole="text"
-				accessibilityValue={{ text: formattedAmount }}
-			>
-				{formattedCurrency}
-				{formattedAmount}
-			</ThemedText>
+			<ActivityListItem.Description description={description} />
+			<ActivityListItem.Amount amount={formattedAmount} />
 		</View>
 	);
 }
+
+ActivityListItem.Description = function Description({
+	description,
+}: {
+	description: string;
+}) {
+	return (
+		<ThemedText
+			accessibilityLabel="Activity description"
+			accessibilityRole="text"
+			accessibilityValue={{ text: description }}
+		>
+			{description}
+		</ThemedText>
+	);
+};
+
+ActivityListItem.Amount = function Amount({ amount }: { amount: string }) {
+	return (
+		<ThemedText
+			type="defaultSemiBold"
+			style={[
+				styles.amount,
+				Number(amount) > 0 ? styles.positiveAmount : styles.negativeAmount,
+			]}
+			accessibilityLabel="Activity amount"
+			accessibilityRole="text"
+			accessibilityValue={{ text: amount }}
+		>
+			{amount}
+		</ThemedText>
+	);
+};
 
 const styles = StyleSheet.create({
 	container: {
