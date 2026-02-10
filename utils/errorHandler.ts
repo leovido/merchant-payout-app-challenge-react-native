44ba2cde2ba07abbf691eae9f206993ec5707e08
Christian Ray Leovido <18484997+leovido@users.noreply.github.com>
2026-02-10 13:11:57 +0100

    fix: error messaging for iOS

--- utils/errorHandler.ts
+++ utils/errorHandler.ts
@@ -9,9 +9,30 @@ function isApiError(error: unknown): error is MerchantApiError {
 	return e.data.error !== undefined;
 }
 
+function parseBiometricsError(error: unknown): string {
+	if (error instanceof Error) {
+		if (error.message === "BIOMETRICS_NOT_ENROLLED") {
+			return "Biometrics not enrolled";
+		} else if (error.message === "BIOMETRICS_AUTH_FAILED") {
+			return "Authentication failed";
+		} else if (error.message === "BIOMETRICS_USER_CANCEL") {
+			return "Biometric authentication was cancelled";
+		} else if (error.message === "BIOMETRICS_UNKNOWN_ERROR") {
+			return "Unknown error";
+		}
+		return error.message;
+	} else {
+		return DEFAULT_MESSAGE;
+	}
+}
+
 export default function extractErrorMessage(error: unknown): string {
 	if (error instanceof Error) {
-		return error.message;
+		if (error.message.includes("BIOMETRICS")) {
+			return parseBiometricsError(error);
+		} else {
+			return error.message;
+		}
 	} else if (isApiError(error)) {
 		return (error as MerchantApiError).data.error;
 	} else {
